eed3d17437b2e3b20ea54b7ff6d2f84334eb5190
hoang <hoangcool0988@gmail.com>
2024-06-03 13:09:56 +0700

    fix vercel

--- flask_api.py
+++ flask_api.py
@@ -1,8 +1,9 @@
-from flask import Flask,  jsonify
+from flask import Flask, jsonify
 
 from routes.webserver_route import webserver_route
 from routes.detect_api import detect_api
 from routes.process_api import process_api
+
 app = Flask(__name__)
 
 app.config['SECRET_KEY'] = 'hoangdev'
@@ -11,6 +12,7 @@ app.config['UPLOAD_FOLDER'] = 'static/files'
 app.register_blueprint(webserver_route)
 app.register_blueprint(detect_api)
 app.register_blueprint(process_api)
+
 
 @app.errorhandler(404)
 def resource_not_found(e):
